8a5a7014c82fbff0ec97c73eb66c0c27d886dde5
Himalay Mohanlal Joriwal <himalayj@amazon.com>
2022-03-10 17:31:28 -0800

    Static linking

---
+++ Cherno/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    bool x = false;
+    char c = 65;
+    int xx = 12;
+    float ff = 5.5;
+    double dd = 5.5;
+
+    cout << sizeof(x) << endl;
+
+    cout << c << endl;
+    cout << sizeof(ff) << endl;
+    cout << sizeof(dd) << endl;
+    cout << sizeof(xx) << endl;
+
+    return 0;
+}
