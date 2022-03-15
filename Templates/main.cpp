0ac9b937922ddf9e3891934f003cd638e5b5a0ef
Himalay Mohanlal Joriwal <himalayj@amazon.com>
2022-03-15 13:23:16 -0700

    Add templates

---
+++ Templates/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template<typename T>
+void Print(T value) {
+    std::cout << value << std::endl;
+}
+
+template<typename T, int N>
+class Array
+{
+private:
+    T m_Array[N];
+public:
+    int GetSize() const {return N; }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Print(5);
+    Print("Hello");
+    Print(5.5f);
+
+    Array<std::string, 50> array;
+    
+    return 0;
+}
