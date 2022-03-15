72458595fa57d6dc0b6b19e5635363f1f79e81c8
Himalay Mohanlal Joriwal <himalayj@amazon.com>
2022-03-15 08:08:32 -0700

    Local Static

---
+++ LocalStatic/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Singleton {
+public:
+    static Singleton& Get() {
+        static Singleton instance;
+        return instance;
+    }
+
+    void Hello() {
+
+    }
+};
+
+void Function() {
+    static int i = 0;
+    i++;
+    std::cout << i << std::endl;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Singleton::Get().Hello();
+
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+
+    return 0;
+}
