d291d289103782f32a91d3c3f55c443acffcfae5
Himalay Mohanlal Joriwal <himalayj@amazon.com>
2022-03-16 11:03:06 -0700

    String view

--- OptimizedString/main.cpp
+++ OptimizedString/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
+#define STRING_VIEW 0
+
+static int allocating_count = 0;
+
 void * operator new(size_t size) {
+    allocating_count++;
     std::cout << "Allocating " << size << " bytes\n";
     return malloc(size);
 }
+
+#if not STRING_VIEW
+void printName(const std::string &name) {
+    std::cout << name << std::endl;
+}
+#else
+void printName(std::string_view name) {
+    std::cout << name << std::endl;
+}
+#endif
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -13,5 +28,20 @@ int main() {
 
     // on heap using new operator.
     std::string name2 = "Himalay Mohanlal Joriwal";
+
+#if not STRING_VIEW
+    std::string firstname = name2.substr(0, 7);
+    std::string lastname = name2.substr(8, 8);
+#else
+    std::string_view firstname(name2.c_str(), 7);
+    std::string_view lastname(name2.c_str()+8, 8);
+#endif
+
+    printName("Himalay Mohanlal Joriwal");
+    printName(firstname);
+    printName(lastname);
+
+    std::cout << "allocating_count= " << allocating_count << std::endl;
+
     return 0;
 }
